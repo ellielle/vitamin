d227c8dbe9e8bce25e4f2f4a4130726516324a8a
ellielle <40385743+ellielle@users.noreply.github.com>
2022-11-10 08:30:50 -0500

    update dependencies

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -3,12 +3,14 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
+export {}
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./../components/HelloWorld.vue')['default']
+    IconCarbonMoon: typeof import('~icons/carbon/moon')['default']
+    IconCarbonSun: typeof import('~icons/carbon/sun')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
 }
-
-export {}
