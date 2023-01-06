b4fcef147ce4dcd7f4ace7c9844308b1d97307ef
ellielle <40385743+ellielle@users.noreply.github.com>
2023-01-06 17:51:37 -0500

    add shim for vscode to recognize vue files in tests

---
+++ test-shims.d.ts
@@ -0,0 +1,10 @@
+// TypeScript in VSCode can't "find" components in tests without the shim.
+declare interface Window {
+  // extend the window
+}
+
+declare module '*.vue' {
+  import { type DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
